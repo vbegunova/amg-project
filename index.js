17414d4df17bf78734c4cca9ab2c2432a661254b
vbegunova <veronikabegunova42@gmail.com>
2023-12-15 15:12:59 -0600

    add modals

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-const serviceLink = document.querySelector('.services-menu');
-const arrowIcon = document.querySelector('.services-arrow-down');
-const servicesMenu = document.querySelector('.header-services-list');
+const serviceLink = document.querySelector(".services-menu");
+const arrowIcon = document.querySelector(".services-arrow-down");
+const servicesMenu = document.querySelector(".header-services-list");
 
 function toggleMenu() {
-    servicesMenu.classList.toggle('is-open');
-    if (servicesMenu.classList.contains('is-open')) {
-        arrowIcon.style.transform = 'rotate(180deg)';
-    } else {
-        arrowIcon.style.transform = 'rotate(0deg)';
-    }
-};
+  servicesMenu.classList.toggle("is-open");
+  if (servicesMenu.classList.contains("is-open")) {
+    arrowIcon.style.transform = "rotate(180deg)";
+  } else {
+    arrowIcon.style.transform = "rotate(0deg)";
+  }
+}
 
-serviceLink.addEventListener('click', toggleMenu);
+serviceLink.addEventListener("click", toggleMenu);
 
-const mobileServiceItem= document.querySelector('.mobile-menu-item.service');
-const mobileServiceLink = document.querySelector('.mobile-menu-link.service');
-const mobileServiceList = document.querySelector('.mobile-services-list');
-const mobileArrowIcon = document.querySelector('.mobile-menu-item .services-arrow-down');
-const mobileServicesMenu = document.querySelector('.mobile-services-list');
+const mobileServiceItem = document.querySelector(".mobile-menu-item.service");
+const mobileServiceLink = document.querySelector(".mobile-menu-link.service");
+const mobileServiceList = document.querySelector(".mobile-services-list");
+const mobileArrowIcon = document.querySelector(
+  ".mobile-menu-item .services-arrow-down"
+);
+const mobileServicesMenu = document.querySelector(".mobile-services-list");
 
 function toggleMobileServices() {
-    mobileServiceItem.classList.toggle('is-open');
-    mobileServiceList.classList.toggle('is-open');
-    if (mobileServiceList.classList.contains('is-open')) {
-        mobileArrowIcon.style.transform = 'rotate(180deg)';
-    } else {
-        mobileArrowIcon.style.transform = 'rotate(0deg)';
-    }
-};
+  mobileServiceItem.classList.toggle("is-open");
+  mobileServiceList.classList.toggle("is-open");
+  if (mobileServiceList.classList.contains("is-open")) {
+    mobileArrowIcon.style.transform = "rotate(180deg)";
+  } else {
+    mobileArrowIcon.style.transform = "rotate(0deg)";
+  }
+}
 
-mobileServiceLink.addEventListener('click', toggleMobileServices);
+mobileServiceLink.addEventListener("click", toggleMobileServices);
+
+const openBtn = document.querySelectorAll(".more-info");
+const closeBtn = document.querySelector(".close-modal-btn");
+const modal = document.querySelector(".backdrop");
+console.log(closeBtn);
+
+openBtn.forEach((button) => button.addEventListener("click", toggleModal));
+
+function toggleModal() {
+  modal.classList.toggle("is-hidden");
+  document.body.style.overflow = modal.classList.contains("is-hidden")
+    ? "auto"
+    : "hidden";
+}
+
+// openBtn.addEventListener('click', toggleModal);
+closeBtn.addEventListener("click", toggleModal);
